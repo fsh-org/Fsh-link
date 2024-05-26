fe90e61bc77dc1f1e7b7f068782f3a355bf4fb55
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-26 10:37:19 +0000

    Make gud

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const fs = require('fs');
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const PORT = 3000;
-
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var path = require('path');
+const fs = require('fs');
+
+const PORT = 20208;
 const app = express();
-const Database = require("easy-json-database")
+app.use(cors());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  let orig = res.send;
+  function mod(text) {
+    return text.replace(/\{\{[^¬]+?\}\}/g, function(match){
+      let re;
+      try {
+        re = eval(match.replace('{{','').replace('}}','').trim());
+      } catch (err) {
+        re = 'Error';
+        console.log('Err: '+err)
+      }
+      return re;
+    })
+  }
+  res.send = function(){
+    arguments[0] = mod(arguments[0]);
+    orig.apply(res, arguments)
+  };
+  next();
+})
+
+const { DB } = require("fshdb")
+const links = new DB('databases/links.json')
 
 process.on('uncaughtException', function(err) {
   console.log('Error!');
@@ -25,67 +50,13 @@ function makeid(length) {
   return result;
 }
 
-var what, uri;
-
-async function cache_url(what, res) {
-  try {
-    const data = await ogs({ url: url.get(what) });
-
-    const {
-      twitterTitle,
-      twitterDescription,
-      twitterImage
-    } = data.result;
-    const {
-      ogTitle,
-      ogDescription,
-      ogImage
-    } = data.result;
-
-    let par = {}
-    if (twitterTitle) {
-      par.title = twitterTitle;
-    } else {
-      par.title = ogTitle;
-    }
-    if (twitterDescription) {
-      par.description = twitterDescription;
-    } else {
-      par.description = ogDescription;
-    }
-    if (twitterImage) {
-      par.image = twitterImage[0].url;
-    } else {
-      par.image = ogImage[0].url;
-    }
-      
-    time.set(what, Math.floor(new Date().getTime() / 1000));
-    cache.set(what, par)
-  } catch (error) {
-    // err
-    //res.send('Error')
-  }
-}
-
-const ogs = require('open-graph-scraper');
-
-const url = new Database('./url.json')
-const cache = new Database('./cache.json')
-const time = new Database('./time.json')
-
-app.use(cors());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
-
 app.get('/', async function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'pages/index.html'))
 })
     
-app.get('/create', async function(req, res) {
+app.post('/create', async function(req, res) {
   uri = req.query['url'];
-  if (uri.length < 5) {
+  if (uri?.length < 5) {
     res.status(400)
     res.json({
       err: true,
@@ -104,109 +75,58 @@ app.get('/create', async function(req, res) {
   if (!uri.includes('://')) {
     uri = 'https://' + uri;
   }
-  let json = JSON.parse(fs.readFileSync('./url.json'));
-  if (Object.values(json).includes(uri)) {
-    res.json({url: `https://link.fsh.plus/${Object.keys(json)[Object.values(json).indexOf(uri)]}`})
-  } else {
-    let code = makeid(6);
-    while (url.has(code)) {
-      code = makeid(6);
-    }
-    url.set(code, uri);
-    res.json({url: `https://link.fsh.plus/${code}`})
+  let code = makeid(6);
+  while (links.has(code)) {
+    code = makeid(6);
   }
+  links.set(code, {
+    url: uri,
+    time: ((Number(req.query['time']) || 0)===0?0:(Math.floor(Date.now()/1000)+(Number(req.query['time']) || 0))),
+    uses: Number(req.query['uses']) || 0,
+  });
+  res.json({
+    url: `https://link.fsh.plus/${code}`
+  })
 })
-
 app.get("/get/:id", async function(req, res) {
   res.json({
-    link: url.get(req.params['id'])
+    link: links.get(req.params['id']).url
   })
 })
 
-app.get('/services/oembed', async function(req, res) {
-  let co = req.query["url"].split("://")[1].split("/")[1]
-  if (co == "") {
-    res.json({
-      version: "1.1",
-      type: "link",
-      title: 'Fsh link'
-    })
-    return;
-  }
-  if (url.has(String(co))) {
-    if (time.has(co)) {
-      if ((Math.floor(new Date().getTime() / 1000)) > time.get(co) + 1200) {
-        cache_url(co,res);
-      }
-    } else {
-      cache_url(co,res);
-    }
-    if (cache.has(co)) {
-      res.json({
-        version: "1.0",
-        type: "link",
-        title: cache.get(co).title || url.get(co).split("://")[1].split("/")[0],
-        provider_name: "Fsh link",
-        provider_url: "http://link.fsh.plus"
-      })
-    } else {
-      res.json({
-        version: "1.0",
-        type: "link",
-        title: url.get(co).split("://")[1].split("/")[0],
-        provider_name: "Fsh link",
-        provider_url: "http://link.fsh.plus"
-      })
-    }
-  } else {
-    res.status(404)
-    res.send("Error")
-  }
-})
-
 app.get("/robots.txt", async function(req, res) {
-  res.sendFile(path.join(__dirname, 'robots.txt'))
+  res.sendFile(path.join(__dirname, 'pages/robots.txt'))
 })
   
 app.get('/:id', async function(req, res) {
-  if (url.has(req.params['id'])) {
-    if (time.has(req.params['id'])) {
-      if ((Math.floor(new Date().getTime() / 1000)) > time.get(req.params['id']) + 1200) {
-        cache_url(req.params['id'],res);
+  if (links.has(req.params['id'])) {
+    if (links.get(req.params['id']).time !== 0) {
+      if ((Date.now()/1000)>links.get(req.params['id']).time) {
+        links.remove(req.params['id']);
+        res.sendFile(path.join(__dirname, 'pages/notfound.html'));
+        return;
       }
-    } else {
-      cache_url(req.params['id'],res);
     }
-
-    let fil = fs.readFileSync('link.html', 'utf8');
-    fil = fil
-      .replaceAll('[URL]', url.get(req.params['id']))
-      .replaceAll('[ID]', req.params['id']);
-    try {
-      fil = fil
-        .replaceAll('[NAME]', cache.get(req.params['id']).title)
-    } catch (err) {
-      fil = fil
-        .replaceAll('[NAME]', url.get(req.params['id']).split("://")[1].split("/")[0])
+    if (req.get('User-Agent').includes('bot')) {
+      res.redirect(links.get(req.params['id']).url);
+      return;
     }
-    try {
-      fil = fil
-        .replaceAll('[DESC]', (cache.get(req.params['id']).description || "None"))
-        .replaceAll('[IMG]', (cache.get(req.params['id']).image || "None"))
-    } catch (err) {
-      fil = fil
-        .replaceAll('[DESC]', "No description")
+    res.send(fs.readFileSync('pages/link.html', 'utf8'))
+    if (links.get(req.params['id']).uses !== 0) {
+      if (links.get(req.params['id']).uses === 1) {
+        links.remove(req.params['id']);
+        return;
+      }
+      links.set(req.params['id']+'.uses', links.get(req.params['id']).uses-1)
     }
-      
-    res.send(fil)
   } else {
-    res.sendFile(path.join(__dirname, 'notfound.html'))
+    res.sendFile(path.join(__dirname, 'pages/notfound.html'))
   }
 })
   
 app.use(function(req, res) {
   res.status(404)
-  res.sendFile(path.join(__dirname, 'error.html'))
+  res.sendFile(path.join(__dirname, 'pages/404.html'))
 })
 
 app.listen(PORT, function(){console.log('listening at '+PORT)});
